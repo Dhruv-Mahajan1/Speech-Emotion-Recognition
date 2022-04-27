a2cfd755a4ad8666ba9c931974a4a2b58803a292
Dhruv-Mahajan <dhruv.mahajan001@gmail.com>
2022-04-27 23:16:56 +0530

    Complete Backend

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from contextlib import AsyncExitStack
 from flask import Flask,render_template,request,redirect
-
+import numpy as np
+import librosa
 import pickle
 
-model = pickle.load(open('model_pickle','rb'))
+model = pickle.load(open('modellgbm.pkl','rb'))
 print(model)
+class SpeechEmotionRecognition:
+  def __init__(self,filename,n_mfcc):
+    self.filename=filename
+    self.n_mfcc=n_mfcc
+
+  def ExtractFeatures(self):
+    self.y, self.samplingrate = librosa.load(self.filename, duration=3, offset=0.5)
+    self.mfcc = np.mean(librosa.feature.mfcc(y=self.y, sr=self.samplingrate, n_mfcc=self.n_mfcc).T, axis=0)
+    self.mfcc=np.array(self.mfcc)
 app=Flask(__name__)
 @app.route("/",methods=["GET", "POST"])
 def hello():
@@ -17,7 +27,15 @@ def hello():
         file = request.files["file"]
         if file.filename == "":
             return redirect(request.url)
-        ans='33333'
+        if file:
+            print(file)
+            n_mfcc=40
+            obj=SpeechEmotionRecognition(file,n_mfcc)
+            obj.ExtractFeatures()
+            z=obj.mfcc
+            ypr=model.predict([z])
+            print(ypr)
+        ans=ypr[0]
 
 
     return render_template("index.html",ans=ans)
--- model.py
+++ model.py
@@ -1,4 +1,5 @@
 # import IPython
+from pyexpat import model
 import numpy as np
 import wave
 import warnings
@@ -7,3 +8,40 @@ import os
 import soundfile
 import librosa
 import glob
+import pickle
+import lightgbm
+import joblib
+from lightgbm import LGBMClassifier
+from sklearn.metrics import accuracy_score as adr
+import pandas as pd
+
+
+class SpeechEmotionRecognition:
+  def __init__(self,filename,n_mfcc):
+    self.filename=filename
+    self.n_mfcc=n_mfcc
+
+  def ExtractFeatures(self):
+    self.y, self.samplingrate = librosa.load(self.filename, duration=3, offset=0.5)
+    self.mfcc = np.mean(librosa.feature.mfcc(y=self.y, sr=self.samplingrate, n_mfcc=self.n_mfcc).T, axis=0)
+    self.mfcc=np.array(self.mfcc)
+
+Dataset=pd.read_csv('X - X.csv')
+X= Dataset.drop(labels=['label'],axis=1)
+Y=Dataset.iloc[:,40]
+X=X.to_numpy()
+Y=Y.to_numpy()
+model = lightgbm.LGBMClassifier(num_leaves=30,subsample=0.8,max_depth=5,n_estimators=400,learning_rate=0.05,reg_lambda=1.2,reg_alpha=1.2)
+model.fit(X,Y)
+
+filename='03-01-01-01-01-01-01.wav'
+n_mfcc=40
+obj=SpeechEmotionRecognition(filename,n_mfcc)
+obj.ExtractFeatures()
+
+z=obj.mfcc
+z.shape
+ypr=model.predict([z])
+print(ypr)
+pickle.dump(model, open('modellgbm.pkl','wb'))
+
